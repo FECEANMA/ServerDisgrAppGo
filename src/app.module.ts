3a7f501d699c0df25b070e2eb80043d7f92b4562
FECEANMA <fernandoencalada921@gmail.com>
2026-02-11 04:39:20 -0500

    feat: add url video

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -8,8 +11,20 @@ import { AulasController } from './aulas/aulas.controller';
 import { AulasModule } from './aulas/aulas.module';
 import { EstudianteModule } from './estudiante/estudiante.module';
 import { PracticaModule } from './practica/practica.module';
+
 @Module({
-  imports: [PrismaModule, DocentesModule, AulasModule, EstudianteModule, PracticaModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'resources'), // carpeta resources en la raíz
+      serveRoot: '/resources', // cómo se verá en la URL
+    }),
+
+    PrismaModule,
+    DocentesModule,
+    AulasModule,
+    EstudianteModule,
+    PracticaModule,
+  ],
   controllers: [AppController, AulasController],
   providers: [AppService, AulasService],
 })
